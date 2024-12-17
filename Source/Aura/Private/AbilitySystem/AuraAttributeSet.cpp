f92554635345830b3d19083e97c87402f41a74c5
Zach Sykes <zsykes21@gmail.com>
2024-12-17 13:43:17 -0700

    Set base attribute numbers.

--- Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
+++ Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
@@ -2,22 +2,28 @@
 
 
 #include "AbilitySystem/AuraAttributeSet.h"
-#include "AbilitySystemComponent.h"
 #include "Net/UnrealNetwork.h"
 
 
 UAuraAttributeSet::UAuraAttributeSet()
 {
+	InitHealth(100.f);
+	InitMaxHealth(100.f);
+	InitMana(50.f);
+	InitMaxMana(50.f);
 }
 
 void UAuraAttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
 {
 	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
 
-	DOREPLIFETIME_CONDITION(UAuraAttributeSet, Health, COND_None, REPNOTIFY_Always);
-	DOREPLIFETIME_CONDITION(UAuraAttributeSet, MaxHealth, COND_None, REPNOTIFY_Always);
-	DOREPLIFETIME_CONDITION(UAuraAttributeSet, Mana, COND_None, REPNOTIFY_Always);
-	DOREPLIFETIME_CONDITION(UAuraAttributeSet, MaxMana, COND_None, REPNOTIFY_Always);
+	FDoRepLifetimeParams LifetimeParams;
+	LifetimeParams.Condition = COND_None;
+
+	DOREPLIFETIME_WITH_PARAMS(UAuraAttributeSet, Health, LifetimeParams);
+	DOREPLIFETIME_WITH_PARAMS(UAuraAttributeSet, MaxHealth, LifetimeParams);
+	DOREPLIFETIME_WITH_PARAMS(UAuraAttributeSet, Mana, LifetimeParams);
+	DOREPLIFETIME_WITH_PARAMS(UAuraAttributeSet, MaxMana, LifetimeParams);
 }
 
 void UAuraAttributeSet::OnRep_Health(const FGameplayAttributeData& OldHealth) const
